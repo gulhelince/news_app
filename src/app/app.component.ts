5d3b1aac41ac29d5c790548a830f5888aeedd87a
Gulsen <gulhelince@hotmail.com>
2024-10-19 02:48:03 +0300

    news_app sayfa oluşturuldu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component,ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
   title = 'news_app';
+  @ViewChild(MatSidenav) sideNav!: MatSidenav;
+
+  constructor(private observer : BreakpointObserver, private cdr: ChangeDetectorRef){}
+  
+  ngAfterViewInit(): void {
+    this.sideNav.opened = true;
+    
+    this.observer.observe(['(max-width:787px)'])
+    .subscribe((res)=>{
+      if(res?.matches){
+        this.sideNav.mode = "over";
+        this.sideNav.close();
+      }else{
+        this.sideNav.mode = "side";
+        this.sideNav.open();
+      }
+    });
+    this.cdr.detectChanges();
+  }
+
 }
